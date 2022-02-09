c6d4b51aaf5f0cd1e109ba28e85c48571bcb255f
Carlos Lobo <carloscamilo.loboulloque@gmail.com>
2022-02-09 13:31:01 -0500

    Refactor barebones library scripts

--- templates/libraries/no-ci-cd/scripts/watch-tests.sh
+++ templates/libraries/no-ci-cd/scripts/watch-tests.sh
@@ -3,61 +3,67 @@
 GREEN="\033[0;32m";
 END_COLOR="\033[0m";
 
-function log() {
-    echo -e "${1}";
+function avalog() {
+  message=${1};
+
+  echo -e "${GREEN}[Avalon]${END_COLOR} - $(date +"%m-%d-%Y, %r") - ${message}";
 }
 
-log "${GREEN}[Avalon]${END_COLOR} - $(date +"%m-%d-%Y, %r") - ${GREEN}🚥 Running tests...${END_COLOR}";
+function bootstrap() {
+  avalog "${GREEN}🚥 Running tests...${END_COLOR}";
 
-# ███████╗███████╗████████╗██╗   ██╗██████╗ 
-# ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
-# ███████╗█████╗     ██║   ██║   ██║██████╔╝
-# ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
-# ███████║███████╗   ██║   ╚██████╔╝██║     
-# ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
+  # ███████╗███████╗████████╗██╗   ██╗██████╗ 
+  # ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+  # ███████╗█████╗     ██║   ██║   ██║██████╔╝
+  # ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+  # ███████║███████╗   ██║   ╚██████╔╝██║     
+  # ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
 
-projectName="{{projectName}}";
-testImageName="${projectName}-test-image";
-testDockerFilePath="./docker/watch-tests.Dockerfile";
-testContainerName="${projectName}-test-container";
-sourceCodePath="$(pwd)/library";
-sourceCodePathWorkdir="/${projectName}";
+  projectName="{{projectName}}";
+  testImageName="${projectName}-test-image";
+  testDockerFilePath="./docker/watch-tests.Dockerfile";
+  testContainerName="${projectName}-test-container";
+  sourceCodePath="$(pwd)/library";
+  sourceCodePathWorkdir="/${projectName}";
 
-# Git volume.
-gitVolumePath="$(pwd)/.git";
-gitContainerPath="/${projectName}/.git";
+  # Git volume.
+  gitVolumePath="$(pwd)/.git";
+  gitContainerPath="/${projectName}/.git";
 
-# Node modules volume.
-nodeModulesVolumeName="${projectName}-node_modules";
-nodeModulesContainerPath="/node_modules";
+  # Node modules volume.
+  nodeModulesVolumeName="${projectName}-node_modules";
+  nodeModulesContainerPath="/node_modules";
 
-# ███████╗██╗  ██╗███████╗ ██████╗██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗
-# ██╔════╝╚██╗██╔╝██╔════╝██╔════╝██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║
-# █████╗   ╚███╔╝ █████╗  ██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║
-# ██╔══╝   ██╔██╗ ██╔══╝  ██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║
-# ███████╗██╔╝ ██╗███████╗╚██████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║
-# ╚══════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+  # ███████╗██╗  ██╗███████╗ ██████╗██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗
+  # ██╔════╝╚██╗██╔╝██╔════╝██╔════╝██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║
+  # █████╗   ╚███╔╝ █████╗  ██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║
+  # ██╔══╝   ██╔██╗ ██╔══╝  ██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║
+  # ███████╗██╔╝ ██╗███████╗╚██████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║
+  # ╚══════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
 
-# Pre-execution cleanup.
-docker container rm ${testContainerName} &> /dev/null;
-docker image rm ${testImageName} &> /dev/null;
+  # Pre-execution cleanup.
+  docker container rm ${testContainerName} &> /dev/null;
+  docker image rm ${testImageName} &> /dev/null;
 
-# Create an image to run a "start development mode" command.
-docker image build \
-  --file ${testDockerFilePath} \
-  --tag ${testImageName} \
-  .;
+  # Create an image to run a "start development mode" command.
+  docker image build \
+    --file ${testDockerFilePath} \
+    --tag ${testImageName} \
+    .;
 
-# Create an image to run a "run tests" command.
-docker container run \
-  --rm \
-  --interactive \
-  --tty \
-  -v ${nodeModulesVolumeName}:${nodeModulesContainerPath} \
-  -v ${sourceCodePath}:${sourceCodePathWorkdir} \
-  -v ${gitVolumePath}:${gitContainerPath} \
-  --name ${testContainerName} \
-  ${testImageName};
+  # Create an image to run a "run tests" command.
+  docker container run \
+    --rm \
+    --interactive \
+    --tty \
+    -v ${nodeModulesVolumeName}:${nodeModulesContainerPath} \
+    -v ${sourceCodePath}:${sourceCodePathWorkdir} \
+    -v ${gitVolumePath}:${gitContainerPath} \
+    --name ${testContainerName} \
+    ${testImageName};
 
-# Post-execution cleanup
-docker image rm ${testImageName} &> /dev/null;
+  # Post-execution cleanup
+  docker image rm ${testImageName} &> /dev/null;
+}
+
+bootstrap $@;
