43b6555b0e0a389e46691dddadcabcf6dacb5274
Carlos Lobo <carloscamilo.loboulloque@gmail.com>
2022-02-09 13:23:25 -0500

    Refactor github-ci-cd library scripts

--- templates/libraries/github-ci-cd/scripts/install.sh
+++ templates/libraries/github-ci-cd/scripts/install.sh
@@ -3,55 +3,60 @@
 GREEN="\033[0;32m";
 END_COLOR="\033[0m";
 
-function log() {
-    echo -e "${1}";
+function avalog() {
+  message=${1};
+
+  echo -e "${GREEN}[Avalon]${END_COLOR} - $(date +"%m-%d-%Y, %r") - ${message}";
 }
 
-log "${GREEN}[Avalon]${END_COLOR} - $(date +"%m-%d-%Y, %r") - ${GREEN}🧰 Installing project...${END_COLOR}";
+function bootstrap() {
+  avalog "${GREEN}🧰 Installing project...${END_COLOR}";
 
-# ███████╗███████╗████████╗██╗   ██╗██████╗ 
-# ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
-# ███████╗█████╗     ██║   ██║   ██║██████╔╝
-# ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
-# ███████║███████╗   ██║   ╚██████╔╝██║     
-# ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
+  # ███████╗███████╗████████╗██╗   ██╗██████╗ 
+  # ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+  # ███████╗█████╗     ██║   ██║   ██║██████╔╝
+  # ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+  # ███████║███████╗   ██║   ╚██████╔╝██║     
+  # ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝
 
-projectName="{{projectName}}";
-imageName="${projectName}-installation-image";
-dockerFilePath="./docker/install.Dockerfile";
-containerName="${projectName}-installation-container";
+  projectName="{{projectName}}";
+  imageName="${projectName}-installation-image";
+  dockerFilePath="./docker/install.Dockerfile";
+  containerName="${projectName}-installation-container";
 
-# Node modules volume.
-nodeModulesVolumeName="${projectName}-node_modules";
-nodeModulesContainerPath="/node_modules";
+  # Node modules volume.
+  nodeModulesVolumeName="${projectName}-node_modules";
+  nodeModulesContainerPath="/node_modules";
 
-# ███████╗██╗  ██╗███████╗ ██████╗██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗
-# ██╔════╝╚██╗██╔╝██╔════╝██╔════╝██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║
-# █████╗   ╚███╔╝ █████╗  ██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║
-# ██╔══╝   ██╔██╗ ██╔══╝  ██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║
-# ███████╗██╔╝ ██╗███████╗╚██████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║
-# ╚══════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ 
+  # ███████╗██╗  ██╗███████╗ ██████╗██╗   ██╗████████╗██╗ ██████╗ ███╗   ██╗
+  # ██╔════╝╚██╗██╔╝██╔════╝██╔════╝██║   ██║╚══██╔══╝██║██╔═══██╗████╗  ██║
+  # █████╗   ╚███╔╝ █████╗  ██║     ██║   ██║   ██║   ██║██║   ██║██╔██╗ ██║
+  # ██╔══╝   ██╔██╗ ██╔══╝  ██║     ██║   ██║   ██║   ██║██║   ██║██║╚██╗██║
+  # ███████╗██╔╝ ██╗███████╗╚██████╗╚██████╔╝   ██║   ██║╚██████╔╝██║ ╚████║
+  # ╚══════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝    ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ 
 
-# Pre-execution cleanup.
-docker container rm ${containerName} &> /dev/null;
-docker volume rm ${nodeModulesVolumeName} &> /dev/null;
-docker image rm ${imageName} &> /dev/null;
+  # Pre-execution cleanup.
+  docker container rm ${containerName} &> /dev/null;
+  docker volume rm ${nodeModulesVolumeName} &> /dev/null;
+  docker image rm ${imageName} &> /dev/null;
 
-# Create an image to run a "install project" command.
-docker image build \
-  --file ${dockerFilePath} \
-  --tag ${imageName} \
-  .;
+  # Create an image to run a "install project" command.
+  docker image build \
+    --file ${dockerFilePath} \
+    --tag ${imageName} \
+    .;
 
-# Run the "install project" command container.
-docker container run \
-  --rm \
-  --tty \
-  -v ${nodeModulesVolumeName}:${nodeModulesContainerPath} \
-  --name ${containerName} \
-  ${imageName};
+  # Run the "install project" command container.
+  docker container run \
+    --rm \
+    --tty \
+    -v ${nodeModulesVolumeName}:${nodeModulesContainerPath} \
+    --name ${containerName} \
+    ${imageName};
 
-# Post-execution cleanup.
-docker container rm ${containerName} &> /dev/null;
-docker image rm ${imageName} &> /dev/null;
- 
+  # Post-execution cleanup.
+  docker container rm ${containerName} &> /dev/null;
+  docker image rm ${imageName} &> /dev/null;
+}
+
+bootstrap $@;
